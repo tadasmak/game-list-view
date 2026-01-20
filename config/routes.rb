60a646c4a4637ed0a0949c9e661dcebef5dec102
Tadas Maksimavicius <tadasmak@gmail.com>
2026-01-21 01:34:10 +0200

    Assign GET /list endpoint for fetching games list

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   get "*path", to: "pages#index", constraints: ->(req) { !req.xhr? && req.format.html? }
 
+  get "list", to: "games#index"
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
