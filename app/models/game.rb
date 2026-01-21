59076066d2c822f7c0eb094f8f58bca918329e1f
Tadas Maksimavicius <tadasmak@gmail.com>
2026-01-21 14:09:47 +0200

    Define platform enum in Game model

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,2 +1,13 @@
 class Game < ApplicationRecord
+    enum platform: {
+        ea_games: 0,
+        steam: 1,
+        epic_games: 2
+        xbox_live: 3,
+        xbox_series: 4,
+        playstation_4: 5,
+        playstation_5: 6,
+        nintendo_switch: 7,
+        nintendo_switch_2: 8
+    }
 end
