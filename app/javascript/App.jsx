f8e161642e19a1b27facdf87510a67b479f94d7a
Tadas Maksimavicius <tadasmak@gmail.com>
2026-01-22 00:25:06 +0200

    Create GamesList root page

--- app/javascript/App.jsx
+++ app/javascript/App.jsx
@@ -1,33 +1,12 @@
 import React from "react";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
-
-function Home() {
-  return (
-    <div>
-      <h1>Home</h1>
-      <p>Welcome to the game list app!</p>
-    </div>
-  );
-}
-
-function GameList() {
-  return (
-    <div>
-      <h1>Games</h1>
-      <p>List of games will go here</p>
-    </div>
-  );
-}
+import GamesList from "./pages/GamesList";
 
 export default function App() {
   return (
     <BrowserRouter>
-      <nav>
-        <Link to="/">Home</Link> | <Link to="/games">Games</Link>
-      </nav>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/games" element={<GameList />} />
+          <Route path="/" element={<GamesList />} />
       </Routes>
     </BrowserRouter>
   );
