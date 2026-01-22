ce67549e5c5dc3b3fe9d23c2c8196da5b5cc60ab
Tadas Maksimavicius <tadasmak@gmail.com>
2026-01-22 14:01:34 +0200

    Create Navigation component

--- app/javascript/App.jsx
+++ app/javascript/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import Navigation from "./components/Navigation";
 import GamesList from "./pages/GamesList";
 
 export default function App() {
   return (
     <BrowserRouter>
+      <Navigation />
       <Routes>
           <Route path="/" element={<GamesList />} />
       </Routes>
