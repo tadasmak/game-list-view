0370bbf0c3d06c2adcacfbb35106e11f2f786725
Tadas Maksimavicius <tadasmak@gmail.com>
2026-01-22 14:07:08 +0200

    Make base app's text color white

--- app/javascript/components/SearchBar.jsx
+++ app/javascript/components/SearchBar.jsx
@@ -4,10 +4,10 @@ import { SearchIcon, XIcon } from "lucide-react";
 export default function SearchBar() {
     return (
         <div className="flex flex-1 h-14 justify-center items-center border-solid border-1 border-white">
-            <SearchIcon className="text-white mx-3" height={32} width={32} />
-            <input className="w-full h-3/4 text-white text-lg font-bold focus:outline-none" type="text" />
+            <SearchIcon className="mx-3" height={32} width={32} />
+            <input className="w-full h-3/4 text-lg font-bold focus:outline-none" type="text" />
             <div className="ml-auto mr-3">
-                <XIcon className="text-white" height={24} width={24} />
+                <XIcon height={24} width={24} />
             </div>
         </div>
     );
