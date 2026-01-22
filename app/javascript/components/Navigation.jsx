f7daaf632bae41f874cb416617507f7d7dd1c82c
Tadas Maksimavicius <tadasmak@gmail.com>
2026-01-22 14:03:40 +0200

    Add logo and SearchBar into Navigation

--- app/javascript/components/Navigation.jsx
+++ app/javascript/components/Navigation.jsx
@@ -3,6 +3,12 @@ import SearchBar from "./SearchBar";
 
 export default function Navigation() {
     return (
-        <></>
+        <div className="flex height-full width-full py-8">
+            <div className="flex items-center w-3/4">
+                <img src="/images/logo-white.png" className="h-auto mr-4" width="140px" />
+                <SearchBar />
+            </div>
+            <div className="flex justify-end items-center w-1/4">smth</div>
+        </div>
     );
 } 
