b1652ec26422b50e1ebf011c20039ba6fb98bab4
Tadas Maksimavicius <tadasmak@gmail.com>
2026-01-22 23:51:57 +0200

    Finish GameCard component

--- app/javascript/components/GameCard.jsx
+++ app/javascript/components/GameCard.jsx
@@ -1,12 +1,27 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { InfoIcon, HeartIcon } from "lucide-react";
 
 export default function GameContainer(game) {
     return (
         <Link to={`/games/${game.id}`} className="flex flex-col flex-[1_0_21%] border-1 border-[#63E3C1]">
             <img src={game.image_url} className="w-full" />
-            <h2>{game.title}</h2>
-            
+            <div className="bg-[#1F0A4D] text-sm text-[#9F96B7] font-bold px-3 py-2">
+                <div className="leading-6 h-[72px] mb-1">
+                    <h2 className="text-white">{game.title} {game.platform} Key {game.region}</h2>
+                    <p className="text-[#29B194]">{game.region}</p>
+                </div>
+                <p className="">From <span className="line-through">€{game.base_price}</span> <span className="text-[#7CD726]">-{game.discount_percent}%</span></p>
+                <div className="flex items-center text-xl">
+                    <span className="text-white">€{game.current_price}</span>
+                    <InfoIcon className="ml-1" height={32} />
+                </div>
+                <p className="text-[#7CD726]">Cashback: €{game.cashback_amount}</p>
+                <div className="flex items-center">
+                    <HeartIcon height={32} />
+                    <span className="ml-1">{game.likes_count}</span>
+                </div>
+            </div>
         </Link>
     )
 }
