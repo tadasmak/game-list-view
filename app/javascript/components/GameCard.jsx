5f08165f9100751e7a802fab6bf488c396a17e33
Tadas Maksimavicius <tadasmak@gmail.com>
2026-01-23 01:32:28 +0200

    Add platform banner overlay in GameCard component

--- app/javascript/components/GameCard.jsx
+++ app/javascript/components/GameCard.jsx
@@ -5,7 +5,10 @@ import { InfoIcon, HeartIcon } from "lucide-react";
 export default function GameContainer(game) {
     return (
         <Link to={`/games/${game.id}`} className="flex flex-col w-[49%] md:w-[32%] lg:w-[24%] border-1 border-[#63E3C1]">
-            <img src={game.image_url} className="h-full object-cover" />
+            <div className="relative w-full">
+                <img src={game.image_url} className="w-full h-[400px] object-cover" />
+                <div className="absolute flex justify-center items-center bottom-0 h-6 w-full font-bold text-sm bg-black/60">{game.platform}</div>
+            </div>
             <div className="bg-[#1F0A4D] text-sm text-[#9F96B7] font-bold px-3 py-2">
                 <div className="leading-6 h-[72px] mb-1">
                     <h2 className="text-white">{game.title} {game.platform} Key {game.region}</h2>
