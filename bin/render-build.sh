854fc568fef0c001dc5b4bb74c19f5533a867cd7
Tadas Maksimavicius <tadasmak@gmail.com>
2026-01-23 14:55:43 +0200

    Add npm commands to build React.js dependencies in render-build.sh

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -1,7 +1,19 @@
 # exit on error
 set -o errexit
 
+# Install dependencies
 bundle install
-bundle exec rake assets:precompile
-bundle exec rake assets:clean
-bundle exec rake db:migrate
+npm install
+
+# Build JavaScript with esbuild
+npm run build
+
+# Build Tailwind CSS
+bundle exec rails tailwindcss:build
+
+# Precompile Rails assets
+bundle exec rails assets:precompile
+bundle exec rails assets:clean
+
+# Run database migrations
+bundle exec rails db:migrate
